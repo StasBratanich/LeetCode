4b0086ed62051946d8898b70a57f9df0bebfc5f3
Stas Bratanich <bstasb@gmail.com>
2023-12-16 11:01:44 +0200

    704. Binary Search

---
+++ 704. Binary Search/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _704.Binary_Search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { -1, 0, 3, 5, 9, 12 };
+            int target1 = 9;
+            int target2 = 2;
+
+            Console.WriteLine(Search(nums, target1));
+            Console.WriteLine(Search(nums, target2));
+
+            Console.ReadLine();
+        }
+
+        public static int Search(int[] nums, int target)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+
+                if (nums[mid] == target)
+                    return mid;
+                else if (nums[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ 739. Daily Temperatures/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace _739.Daily_Temperatures
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] temperatures1 = { 73, 74, 75, 71, 69, 72, 76, 73 };
+            int[] temperatures2 = { 30, 40, 50, 60 };
+            int[] temperatures3 = { 30, 60, 90 };
+
+            Console.WriteLine("Example 1:");
+            PrintArray(DailyTemperatures(temperatures1));
+
+            Console.WriteLine("Example 2:");
+            PrintArray(DailyTemperatures(temperatures2));
+
+            Console.WriteLine("Example 3:");
+            PrintArray(DailyTemperatures(temperatures3));
+
+            Console.ReadLine();
+        }
+        public static int[] DailyTemperatures(int[] temperatures)
+        {
+            Stack<int> stack = new Stack<int>();
+            int[] answerArr = new int[temperatures.Length];
+
+            
+
+            return answerArr;
+        }
+        public static void PrintArray(int[] arr)
+        {
+            foreach (var num in arr)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
